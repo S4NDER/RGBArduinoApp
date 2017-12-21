fc712b8c46d3afce6207eddc8fe6686af6529fc6
Sander Jansen <sanderjansen1997@hotmail.com>
2017-12-21 12:39:30 +0100

    Items of RecView cannot be swiped away

--- app/src/main/java/com/jansen/sander/carrgbapp/SwipeController.java
+++ app/src/main/java/com/jansen/sander/carrgbapp/SwipeController.java
@@ -1,7 +1,10 @@
 package com.jansen.sander.carrgbapp;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper.Callback;
+import android.view.MotionEvent;
+import android.view.View;
 
 import static android.support.v7.widget.helper.ItemTouchHelper.*;
 
@@ -10,6 +13,9 @@ import static android.support.v7.widget.helper.ItemTouchHelper.*;
  */
 
 public class SwipeController extends Callback {
+
+    boolean swipeBack;
+
     @Override
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         return makeMovementFlags(0, LEFT|RIGHT);
@@ -24,4 +30,37 @@ public class SwipeController extends Callback {
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
 
     }
+
+    @Override
+    public int convertToAbsoluteDirection(int flags, int layoutDirection) {
+        if (swipeBack) {
+            swipeBack = false;
+            return 0;
+        }
+        return super.convertToAbsoluteDirection(flags, layoutDirection);
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+        if (actionState == ACTION_STATE_SWIPE) {
+            setTouchListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+
+    private void setTouchListener(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                  float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                swipeBack = event.getAction() == MotionEvent.ACTION_CANCEL || event.getAction() == MotionEvent.ACTION_UP;
+                return false;
+            }
+        });
+    }
+
+
 }
