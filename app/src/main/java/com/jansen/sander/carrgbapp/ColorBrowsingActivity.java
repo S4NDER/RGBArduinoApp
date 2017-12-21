5339612b2909418b3a3f5d0ce9c3c2e8bcb03be7
Sander Jansen <sanderjansen1997@hotmail.com>
2017-12-21 16:20:21 +0100

    Added 'go back' arrow on load color activity

--- app/src/main/java/com/jansen/sander/carrgbapp/ColorBrowsingActivity.java
+++ app/src/main/java/com/jansen/sander/carrgbapp/ColorBrowsingActivity.java
@@ -1,15 +1,18 @@
 package com.jansen.sander.carrgbapp;
 
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.SeekBar;
 
 import java.util.ArrayList;
@@ -31,6 +34,7 @@ public class ColorBrowsingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_color_browsing);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+        setupActionBar();
 
         getAllSavedColors();
     }
@@ -38,6 +42,24 @@ public class ColorBrowsingActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void getAllSavedColors(){
